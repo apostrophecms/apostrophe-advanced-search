1177122bd0d557305ef51d1b7a06d2271b2ae1f3
Stuart Romanek <stuart@punkave.com>
2018-06-04 18:43:19 -0400

    uh

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # apostrophe-advanced-search
 An advanced search interface for Apostrophe
-tests
---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extend: 'apostrophe-module',
+  moogBundle: {
+    modules: [
+      'apostrophe-advanced-search-global',
+      'apostrophe-advanced-search-widgets'
+    ],
+    directory: 'lib/modules'
+  }
+};
