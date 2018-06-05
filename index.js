c49c4d0038f77b5f1c9319cdae90b7a72758cfd0
Stuart Romanek <stuart@punkave.com>
2018-06-05 16:31:22 -0400

    interface feeling good

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var match = [{
+  slug: 'the-michlin-tire-product',
+  title: 'The Michelin tire product range',
+  excerpt: 'A joyless operas vase comes with it the thought that the hackneyed enemy is a decrease.They were lost without the zippy oyster that composed their close.Their clef was, in this moment, a thirstless hose.Those sides are nothing more than grills',
+  url: 'https://www.michelin.com/eng/michelin-group/products-services/michelin-tires',
+  groups: [{
+      type: 'product',
+      typeLabel: 'Product Page'
+    },
+    {
+      type: 'apostrophe-page',
+      typeLabel: 'Page'
+    },
+  ]
+}];
+
 module.exports = {
   extend: 'apostrophe-module',
   moogBundle: {
@@ -6,5 +22,70 @@ module.exports = {
       'apostrophe-advanced-search-widgets'
     ],
     directory: 'lib/modules'
+  },
+  construct: function(self, options) {
+    self.addHelpers({
+      matches: match.concat(match).concat(match),
+      filters: [{
+          name: 'content',
+          label: 'By Content',
+          items: [{
+              name: 'page',
+              label: 'Pages',
+              url: '??',
+              count: 10
+            },
+            {
+              name: 'products',
+              label: 'Products',
+              url: '??',
+              count: 5
+            },
+            {
+              name: 'profiles',
+              label: 'Profiles',
+              url: '??',
+              count: 3
+            },
+            {
+              name: 'articles',
+              label: 'Articles',
+              url: '??',
+              count: 1
+            }
+          ]
+        },
+        {
+          name: 'another',
+          label: 'By Another One',
+          items: [{
+              name: 'page',
+              label: 'Hello',
+              url: '??',
+              count: 3
+            },
+            {
+              name: 'products',
+              label: 'Goobye',
+              url: '??',
+              count: 3
+            },
+            {
+              name: 'profiles',
+              label: 'Nice to See You',
+              url: '??',
+              count: 99
+            },
+            {
+              name: 'articles',
+              label: 'Hell this is an item',
+              url: '??',
+              count: 59
+            }
+          ]
+        }
+      ]
+
+    });
   }
 };
