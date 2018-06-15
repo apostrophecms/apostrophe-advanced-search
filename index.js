29ad8e7ec0457f0d4b9f5658fa4c31d645c750fd
Stuart Romanek <stuart@punkave.com>
2018-06-15 09:22:32 -0400

    WIP

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-var match = [{
-  slug: 'the-michlin-tire-product',
-  title: 'The Michelin tire product range',
-  excerpt: 'A joyless operas vase comes with it the thought that the hackneyed enemy is a decrease.They were lost without the zippy oyster that composed their close.Their clef was, in this moment, a thirstless hose.Those sides are nothing more than grills',
-  url: 'https://www.michelin.com/eng/michelin-group/products-services/michelin-tires',
-  groups: [{
-      type: 'product',
-      typeLabel: 'Product Page'
-    },
-    {
-      type: 'apostrophe-page',
-      typeLabel: 'Page'
-    },
-  ]
-}];
-
 module.exports = {
   extend: 'apostrophe-module',
   moogBundle: {
     modules: [
       'apostrophe-advanced-search-global',
-      'apostrophe-advanced-search-widgets'
+      'apostrophe-advanced-search-widgets',
+      'apostrophe-advanced-search-search'
     ],
     directory: 'lib/modules'
   },
   construct: function(self, options) {
     self.addHelpers({
-      matches: match.concat(match).concat(match),
       filters: [{
           name: 'content',
           label: 'By Content',
